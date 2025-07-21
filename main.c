da623849beab2ec4e8c00f68a4a74da718570b31
stillfingingmyself <skyler0786my@gmail.com>
2025-07-21 15:26:11 +0530

    First upload: sassy grade evaluator in C xD

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    char grade;
+
+    printf("Enter your grade:\n");
+    scanf(" %c", &grade); // Note the space before %c to skip whitespace
+    grade = toupper(grade); // Convert to uppercase after input
+
+    if (grade == 'A')
+        printf("...How can a mortal score so much?\n");
+    else if (grade == 'B')
+        printf("good enough... at least you won't die like who scored D :P\n");
+    else if (grade == 'C')
+        printf("meh...study dumbass!\n");
+    else if (grade == 'D')
+        printf("sorry.. now even i can't save you from your asian parents...  '_'\n");
+    else if (grade == 'F')
+        printf("Press F to pay respects... to your GPA.\n");
+    else
+        printf("bruh, what grading system are you even following??\n");
+
+    return 0;
+}
